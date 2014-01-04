1e88cba31ced98dcc904e687ca7360df08468678
Michael Gunnulfsen <michaelgunnulfsen@Michaels-MacBook-Pro-2.local>
2014-01-04 12:14:30 +0100

    clean up adds

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,6 @@ var auth = require('./middlewares/authorization');
 /**
  * Route middlewares
  */
-
  var userController		= require('../app/controllers/userController');
  var workoutProgram = require('../app/controllers/workoutProgram');
 
@@ -34,54 +33,5 @@ module.exports = function(app, passport){
 			//failureFlash: 'Invalid email or password.'
 		}), userController.session)
 	app.get('/users/:userId', userController.show)
-	app.get('/auth/facebook',
-		passport.authenticate('facebook', {
-			scope: [ 'email', 'user_about_me'],
-			failureRedirect: '/login'
-		}), userController.signin)
-	app.get('/auth/facebook/callback',
-		passport.authenticate('facebook', {
-			failureRedirect: '/login'
-		}), userController.authCallback)
-	app.get('/auth/github',
-		passport.authenticate('github', {
-			failureRedirect: '/login'
-		}), userController.signin)
-	app.get('/auth/github/callback',
-		passport.authenticate('github', {
-			failureRedirect: '/login'
-		}), userController.authCallback)
-	app.get('/auth/twitter',
-		passport.authenticate('twitter', {
-			failureRedirect: '/login'
-		}), userController.signin)
-	app.get('/auth/twitter/callback',
-		passport.authenticate('twitter', {
-			failureRedirect: '/login'
-		}), userController.authCallback)
-	app.get('/auth/google',
-		passport.authenticate('google', {
-			failureRedirect: '/login',
-			scope: [
-			'https://www.googleapis.com/auth/userinfo.profile',
-			'https://www.googleapis.com/auth/userinfo.email'
-			]
-		}), userController.signin)
-	app.get('/auth/google/callback',
-		passport.authenticate('google', {
-			failureRedirect: '/login'
-		}), userController.authCallback)
-	app.get('/auth/linkedin',
-		passport.authenticate('linkedin', {
-			failureRedirect: '/login',
-			scope: [ 
-			'r_emailaddress'
-			]
-		}), userController.signin)
-	app.get('/auth/linkedin/callback',
-		passport.authenticate('linkedin', {
-			failureRedirect: '/login'
-		}), userController.authCallback)
-
 	app.param('userId', userController.user)
 };
