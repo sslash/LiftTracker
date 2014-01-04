4cb3f42193f7c18688d5b617b458a8fd8d40e18c
Michael Gunnulfsen <michaelgunnulfsen@Michaels-MacBook-Pro-2.local>
2014-01-04 13:06:46 +0100

    q

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose'),
 	User = mongoose.model('User'),
-	Q	= require('q'),
+	Q = require('Q'),
 	utils = require('../../lib/utils');
 	WorkoutProgram = require('./workoutProgram');
 
