108b764359c92ae84909144bd9eac4f222b6b45a
Michael Gunnulfsen <michaelgunnulfsen@Michaels-MacBook-Pro-2.local>
2014-01-04 13:08:56 +0100

    sap

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose'),
 	User = mongoose.model('User'),
-	Q = require('Q'),
+	Q = require('q'),
 	utils = require('../../lib/utils');
 	WorkoutProgram = require('./workoutProgram');
 
